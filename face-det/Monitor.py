414c1e0c73c75fd2681b0bc5e4947ec0bdc09b0c
RAJPUTRoCkStAr <sumitsingh9441@gmail.com>
2024-08-29 11:47:01 +0530

    cahnegs

--- face-det/Monitor.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-def monitor():
-    ma = st.button('Monitor Activity', use_container_width=True, type='primary')
-    if ma:
-        st.write('Monitor button clicked')
-
