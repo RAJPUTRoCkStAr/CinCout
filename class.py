b3aa9209a31a79afcfce946eefcce8904d65373d
RAJPUTRoCkStAr <sumitsingh9441@gmail.com>
2024-09-13 23:38:21 +0530

    changes

--- class.py
+++ class.py
@@ -1,5 +1,10 @@
+# from ultralytics import YOLO
+
+# # Load a pre-trained YOLOv8 model
+# model = YOLO('testing/yolov10n.pt')
+# print(model.names)
+import cv2
 from ultralytics import YOLO
 
-# Load a pre-trained YOLOv8 model
-model = YOLO('testing/yolov10n.pt')
-print(model.names)
+print("OpenCV version:", cv2.__version__)
+print("Ultralytics YOLO version:", YOLO._version__)
