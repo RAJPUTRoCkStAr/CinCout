7eb9021696a7f76f51ff9563984f2dfc644ed47a
RAJPUTRoCkStAr <sumitsingh9441@gmail.com>
2024-09-22 19:46:20 +0530

    tts

--- Utils.py
+++ Utils.py
@@ -14,19 +14,12 @@ import sqlite3
 import base64
 import re
 import os
-import soundfile as sf
-import pyttsx3
 from streamlit_TTS import auto_play
 ################################################################
 #text-to-speech
 
 def tts(text):
     auto_play(text)
-    # engine = pyttsx3.init()
-    # engine.setProperty('rate', 150)    # Speed of speech (words per minute)
-    # engine.setProperty('volume', 1.0)  # Volume level (0.0 to 1.0)
-    # # engine.say(text)
-#     engine.runAndWait()
 
 ####################################################################
 
