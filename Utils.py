d492e038df27795d2fb645e77c9b3950c77dbc1c
RAJPUTRoCkStAr <sumitsingh9441@gmail.com>
2024-09-22 19:44:27 +0530

    tts

--- Utils.py
+++ Utils.py
@@ -21,12 +21,12 @@ from streamlit_TTS import auto_play
 #text-to-speech
 
 def tts(text):
-    engine = pyttsx3.init()
-    engine.setProperty('rate', 150)    # Speed of speech (words per minute)
-    engine.setProperty('volume', 1.0)  # Volume level (0.0 to 1.0)
-    # engine.say(text)
     auto_play(text)
-    engine.runAndWait()
+    # engine = pyttsx3.init()
+    # engine.setProperty('rate', 150)    # Speed of speech (words per minute)
+    # engine.setProperty('volume', 1.0)  # Volume level (0.0 to 1.0)
+    # # engine.say(text)
+#     engine.runAndWait()
 
 ####################################################################
 
