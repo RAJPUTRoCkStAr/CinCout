21d8e97723518c573a1c50e324a4183ef53c972a
RAJPUTRoCkStAr <sumitsingh9441@gmail.com>
2024-09-23 12:24:17 +0530

    chnag

--- Utils.py
+++ Utils.py
@@ -20,11 +20,9 @@ from streamlit_TTS import auto_play
 #text-to-speech
 
 def tts(text):
-    auto_play(text)
     engine = pyttsx3.init()
     engine.setProperty('rate', 150)    # Speed of speech (words per minute)
     engine.setProperty('volume', 1.0)  # Volume level (0.0 to 1.0)
-    # engine.say(text)
     engine.runAndWait()
 
 ####################################################################
